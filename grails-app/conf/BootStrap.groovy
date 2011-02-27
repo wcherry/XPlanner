97162cb990217b2ca9f1f0365c1e5ab8de768503
wcherry <wcherry69@gmail.com>
2011-02-27 13:01:16 -0800

    Switched security provide to Shiro

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,28 @@
+import grails.util.Environment
+import org.apache.shiro.crypto.hash.Sha1Hash
+
 class BootStrap {
 
     def init = { servletContext ->
+	
+	    // def wildcardPermission = new JsecPermission(type: "org.jsecurity.authz.permission.WildcardPermission", possibleActions: "*").save()
+        // new JsecUserPermissionRel(user: user1, permission: wildcardPermission, target: "book:list,show", actions: "*").save()
+        // new JsecUserPermissionRel(user: user2, permission: wildcardPermission, target: "book:*", actions: "*").save()
+	
+      switch (Environment.current) {
+      case Environment.DEVELOPMENT:
+		def adminRole = new org.sss.xplanner.Role(name: "Administrator")
+		adminRole.addToPermissions("printer:*:*")
+		adminRole.addToPermissions("admin")
+		adminRole.save()
+	  
+		def user = new org.sss.xplanner.User(username: "admin", passwordHash: new Sha1Hash("password").toHex())
+		user.addToRoles(adminRole)
+        user.addToPermissions("*:*")
+        user.save()
+        break;
+      }
+	
     }
     def destroy = {
     }
---
+++ grails-app/domain/org/sss/xplanner/User.groovy
@@ -0,0 +1,12 @@
+package org.sss.xplanner;
+
+class User {
+    String username
+    String passwordHash
+    
+    static hasMany = [ roles: Role, permissions: String ]
+
+    static constraints = {
+        username(nullable: false, blank: false)
+    }
+}
