280593537b0176b787dee332b99557b3ef013564
William Cherry <wcherry69@gmail.com>
2011-03-29 11:43:19 -0700

    Added TaskStatus and default users to bootstrap in Test enviornment.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,38 +6,51 @@ class BootStrap {
 
     def init = { servletContext ->
       switch (Environment.current) {
-      case Environment.DEVELOPMENT:
-      new TaskStatus(name: "Unassigned", sequence: 0,description: "Unassigned").save()
-      new TaskStatus(name: "In Progress", sequence: 1,description: "na").save()
-      new TaskStatus(name: "Completed",sequence: 2,description: "na").save()
-      new TaskStatus(name: "Postponed",sequence: 3,description: "na").save()
-      new TaskStatus(name: "Canceled",sequence: 4,description: "na").save()
-      def u = new User(username: "admin").save()
-      new User(username: "guest").save()
-      
-      User.metaClass.'static'.loggedOnUser = {
-        return u
+        case Environment.DEVELOPMENT:
+          new TaskStatus(name: "Unassigned", sequence: 0,description: "Unassigned").save()
+          new TaskStatus(name: "In Progress", sequence: 1,description: "na").save()
+          new TaskStatus(name: "Completed",sequence: 2,description: "na").save()
+          new TaskStatus(name: "Postponed",sequence: 3,description: "na").save()
+          new TaskStatus(name: "Canceled",sequence: 4,description: "na").save()
+          def u = new User(username: "admin").save()
+          new User(username: "guest").save()
+          
+          User.metaClass.'static'.loggedOnUser = {
+            return u
+          }
+          
+          
+          
+          def p = new Project(title: "Default", description: "Default").save()
+          def t = new Task(project: p, creator: u, title: "Title #1", effort: 1, iteration: 0)
+          t.save()
+          if(t.hasErrors()) {
+            t.errors.each {
+              println it
+            }
+          }
+          p.addToTasks(t)
+          p.addToTasks(new Task(creator: u,  title: "Title #2", effort: 1, iteration: 0))
+          p.addToTasks(new Task(creator: u,  title: "Title #3", effort: 1, iteration: 0))
+          p.addToTasks(new Task(creator: u,  title: "Title #4", effort: 1, iteration: 0))
+          p.addToTasks(new Task(creator: u,  title: "Title #5", effort: 1, iteration: 0))
+          p.save()
+          break;
+
+        case Environment.TEST:
+          new TaskStatus(name: "Unassigned", sequence: 0,description: "Unassigned").save()
+          new TaskStatus(name: "In Progress", sequence: 1,description: "na").save()
+          new TaskStatus(name: "Completed",sequence: 2,description: "na").save()
+          new TaskStatus(name: "Postponed",sequence: 3,description: "na").save()
+          new TaskStatus(name: "Canceled",sequence: 4,description: "na").save()
+          def u = new User(username: "admin").save()
+          new User(username: "guest").save()
+          
+          User.metaClass.'static'.loggedOnUser = {
+            return u
+          }
+          break;
       }
-      
-      
-      
-      def p = new Project(title: "Default", description: "Default").save()
-      def t = new Task(project: p, creator: u, title: "Title #1", effort: 1, iteration: 0)
-      t.save()
-      if(t.hasErrors()) {
-        t.errors.each {
-          println it
-        }
-      }
-      p.addToTasks(t)
-      p.addToTasks(new Task(creator: u,  title: "Title #2", effort: 1, iteration: 0))
-      p.addToTasks(new Task(creator: u,  title: "Title #3", effort: 1, iteration: 0))
-      p.addToTasks(new Task(creator: u,  title: "Title #4", effort: 1, iteration: 0))
-      p.addToTasks(new Task(creator: u,  title: "Title #5", effort: 1, iteration: 0))
-      p.save()
-      break;
-      }
-	
     }
     def destroy = {
     }
